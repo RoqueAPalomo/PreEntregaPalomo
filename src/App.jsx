b6b0712aef83f9af121437307bdd0b597a3c144a
Roque Alejandro Palomo <apalomo928@gmail.com>
2023-07-05 02:18:33 -0300

    preentrega-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 
 import './App.css'
-import Header from './components/header'
+import Header from './components/header/NavBar'
+import CartWidget from './components/header/CartWidget'
+import ItemListContainer from './components/header/ItemListContainer'
+
 
 function App() {
   
   return (
     <div>
       <Header logo= "Tienda Anime"/>
+      <ItemListContainer/>
+      <CartWidget/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/header/ItemListContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ItemListContainer = () => {
+  return (
+    <div>
+      <nav className="nav">
+                <ul className="menu_items">
+                    <li><a href="">Productos</a></li>
+                    <li><a href="">Categorias</a></li>
+                    <li><a href="">Sobre Nosotros</a></li>
+                    <li><a href="">Contacto</a></li>
+                   
+                </ul>
+            </nav>
+    </div>
+  );
+};
+
+export default ItemListContainer;
