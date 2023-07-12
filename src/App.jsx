cb4fdf1d54a48b212af0b9b3c1fd96b00ca1117c
Roque Alejandro Palomo <apalomo928@gmail.com>
2023-07-12 11:55:11 -0300

    preentrega2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,55 @@
-
+import { useEffect, useState } from 'react';
 import './App.css'
-import Header from './components/header/NavBar'
-import CartWidget from './components/header/CartWidget'
-import ItemListContainer from './components/header/ItemListContainer'
-
+import Header from './components/navbar'
+import Card from './components/producto';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  
+
+//ciclos de vida, promises, asincronía
+
+const [products, setProducts] = useState([]);
+
+useEffect(() => {
+  const getProduct = async () => {
+    try{
+      const response = await fetch('https://64a89377dca581464b85e1d1.mockapi.io/products',{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'Application/jason',
+        },
+      });
+      const data = await response.json();
+      setProducts(data);
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+  getProduct();
+}, [])
+
+
+console.log({products});
   return (
     <div>
-      <Header logo= "Tienda Anime"/>
-      <ItemListContainer/>
-      <CartWidget/>
+      <Header logo="Tienda Shops"/>
+      <div className='card-container'>
+      {
+      products.map((product) => (
+        <Card {...product}/>
+        )
+        )
+      }
+      //routing y navegación
+      <Routes >  
+        <Route path='/' element={<producto />}/>
+      </Routes>
+      </div>
+   
     </div>
+   
   )
 }
 
